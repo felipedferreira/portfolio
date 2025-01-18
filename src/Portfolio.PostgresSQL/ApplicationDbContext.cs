5d4bf8e0e8ca5ed8f51f44e33905a1d0b5bf84df
Felipe Ferreira <felipe.w.ferreira@pwc.com>
2025-01-18 01:04:54 -0500

    Adds EF core code and removes old ADO adapter

---
+++ src/Portfolio.PostgresSQL/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Portfolio.PostgresSQL.Entities;
+
+namespace Portfolio.PostgresSQL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration configuration) : base(options)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<Team> Team { get; set; }
+        public DbSet<Coach> Coaches { get; set; }
+        public DbSet<Player> Players { get; set; }
+
+    }
+}
--- src/Portfolio.PostgresSQL/DependencyInjectionExtensions.cs
+++ src/Portfolio.PostgresSQL/DependencyInjectionExtensions.cs
@@ -1,12 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Portfolio.PostgresSQL;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class DependencyInjectionExtensions
     {
-        public static IServiceCollection AddPostgresSQL(this IServiceCollection services)
+        public static IServiceCollection AddPostgresSQL(this IServiceCollection services, IConfiguration configuration)
         {
-            return services.AddScoped<SqlConnectionHealthCheck>();
+            return services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
         }
     }    
 }
